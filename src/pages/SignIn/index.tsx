ba704df7c98b47678810ea3f48681f190b84a7ee
Bruno Henrique Magro <brunohmagro@hotmail.com>
2021-02-27 10:03:08 -0300

    Implements method signOut

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -6,36 +6,48 @@ import { FiLogIn, FiMail, FiLock } from 'react-icons/fi'
 
 import logoImg from '../../assets/images/logo.svg'
 import { Container, Content, Background } from './styles'
+import getValidationErrors from '../../utils/getValidationErrors'
+import { useAuth } from '../../hooks/AuthContext'
 
 import Input from '../../components/Input'
 import Button from '../../components/Button'
-import getValidationErrors from '../../utils/getValidationErrors'
+
+interface SignInFormData {
+  email: string
+  password: string
+}
 
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null)
-  // eslint-disable-next-line
-  const handleSubmit = useCallback(async (data: object) => {
-    try {
-      formRef.current?.setErrors({})
-      const schema = Yup.object().shape({
-        email: Yup.string()
-          .required('O campo e-mail é obrigatório')
-          .email('Digite um e-mail válido'),
-        password: Yup.string().required('O campo senha é obrigatório'),
-      })
 
-      await schema.validate(data, {
-        abortEarly: false,
-      })
-    } catch (err) {
-      const errors = getValidationErrors(err)
-      formRef.current?.setErrors(errors)
-      // eslint-disable-next-line
-      console.log(JSON.stringify(err))
-      // eslint-disable-next-line
-      console.log(err.inner)
-    }
-  }, [])
+  const { signIn } = useAuth()
+
+  const handleSubmit = useCallback(
+    async (data: SignInFormData) => {
+      try {
+        formRef.current?.setErrors({})
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .required('O campo e-mail é obrigatório')
+            .email('Digite um e-mail válido'),
+          password: Yup.string().required('O campo senha é obrigatório'),
+        })
+
+        await schema.validate(data, {
+          abortEarly: false,
+        })
+
+        signIn({
+          email: data.email,
+          password: data.password,
+        })
+      } catch (err) {
+        const errors = getValidationErrors(err)
+        formRef.current?.setErrors(errors)
+      }
+    },
+    [signIn],
+  )
 
   return (
     <Container>
