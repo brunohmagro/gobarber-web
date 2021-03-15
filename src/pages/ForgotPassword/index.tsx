cda5564bd1d6201c239a6f66b458ac41bc842422
Bruno Henrique Magro <brunohmagro@hotmail.com>
2021-03-15 09:43:54 -0300

    Implementing forgot password page

--- src/pages/ForgotPassword/index.tsx
+++ src/pages/ForgotPassword/index.tsx
@@ -2,51 +2,43 @@ import React, { useCallback, useRef } from 'react'
 import { Form } from '@unform/web'
 import { FormHandles } from '@unform/core'
 import * as Yup from 'yup'
-import { FiLogIn, FiMail, FiLock } from 'react-icons/fi'
-import { Link, useHistory } from 'react-router-dom'
+import { FiLogIn, FiMail } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
 
 import logoImg from '../../assets/images/logo.svg'
 import { Container, Content, Background, AnimationContainer } from './styles'
 import getValidationErrors from '../../utils/getValidationErrors'
-import { useAuth } from '../../hooks/auth'
 import { useToast } from '../../hooks/toast'
 
 import Input from '../../components/Input'
 import Button from '../../components/Button'
 
-interface SignInFormData {
+interface ForgotPasswordFormData {
   email: string
-  password: string
 }
 
-const SignIn: React.FC = () => {
+const ForgotPassword: React.FC = () => {
   const formRef = useRef<FormHandles>(null)
 
-  const { signIn } = useAuth()
   const { addToast } = useToast()
-  const history = useHistory()
 
   const handleSubmit = useCallback(
-    async (data: SignInFormData) => {
+    async (data: ForgotPasswordFormData) => {
       try {
         formRef.current?.setErrors({})
         const schema = Yup.object().shape({
           email: Yup.string()
             .required('O campo e-mail é obrigatório')
             .email('Digite um e-mail válido'),
-          password: Yup.string().required('O campo senha é obrigatório'),
         })
 
         await schema.validate(data, {
           abortEarly: false,
         })
 
-        await signIn({
-          email: data.email,
-          password: data.password,
-        })
+        // RECUPERAÇÃO DE SENHA AQUI
 
-        history.push('/dashboard')
+        // history.push('/dashboard')
       } catch (err) {
         if (err instanceof Yup.ValidationError) {
           const errors = getValidationErrors(err)
@@ -56,13 +48,12 @@ const SignIn: React.FC = () => {
 
         addToast({
           type: 'error',
-          title: 'Erro na autenticação',
-          description:
-            'Ocorreu um erro ao fazer o login, cheque as credenciais',
+          title: 'Erro ao tentar recuperar sua senha',
+          description: 'Ocorreu um erro ao fazer a recuperação de senha',
         })
       }
     },
-    [signIn, addToast, history],
+    [addToast],
   )
 
   return (
@@ -71,27 +62,19 @@ const SignIn: React.FC = () => {
         <AnimationContainer>
           <img src={logoImg} alt="GoBarber" />
           <Form ref={formRef} onSubmit={handleSubmit}>
-            <h1>Faça seu logon</h1>
+            <h1>Recuperar senha</h1>
             <Input
               name="email"
               icon={FiMail}
               type="text"
               placeholder="E-mail"
             />
-            <Input
-              name="password"
-              icon={FiLock}
-              type="password"
-              placeholder="Senha"
-            />
-            <Button type="submit">Entrar</Button>
-
-            <Link to="/esqueci-senha">Esqueci minha senha</Link>
+            <Button type="submit">Recuperar</Button>
           </Form>
 
-          <Link to="/cadastrar">
+          <Link to="/">
             <FiLogIn />
-            Criar conta
+            Voltar ao login
           </Link>
         </AnimationContainer>
       </Content>
@@ -100,4 +83,4 @@ const SignIn: React.FC = () => {
   )
 }
 
-export default SignIn
+export default ForgotPassword
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,12 +5,15 @@ import Route from './Route'
 
 import SignIn from '../pages/SignIn'
 import SignUp from '../pages/SignUp'
+import ForgotPassword from '../pages/ForgotPassword'
+
 import Dashboard from '../pages/Dashboard'
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
     <Route path="/cadastrar" exact component={SignUp} />
+    <Route path="/esqueci-senha" exact component={ForgotPassword} />
 
     <Route path="/dashboard" exact component={Dashboard} isPrivate />
   </Switch>
