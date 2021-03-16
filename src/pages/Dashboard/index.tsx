4a50b83ca0905cd0fb50c8338ac9ec1633218ed0
Bruno Henrique Magro <brunohmagro@hotmail.com>
2021-03-16 16:17:54 -0300

    Implementing schedule in content

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,15 +1,22 @@
 import React from 'react'
-import { FiPower } from 'react-icons/fi'
+import { FiClock, FiPower } from 'react-icons/fi'
 
-import { Container, Header, HeaderContent, Profile } from './styles'
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Calendar,
+} from './styles'
 
 import logoImage from '../../assets/images/logo.svg'
 import { useAuth } from '../../hooks/auth'
 
 const Dashboard: React.FC = () => {
   const { signOut, user } = useAuth()
-
-  console.log(user.avatar_url)
 
   return (
     <Container>
@@ -30,6 +37,31 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img src={user.avatar_url} alt={user.name} />
+
+              <strong>{user.name}</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   )
 }
