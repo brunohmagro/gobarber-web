d91398d3c3a7733c53b5a2bdf9fb65c9aec8d160
Bruno Henrique Magro <brunohmagro@hotmail.com>
2021-03-17 20:11:14 -0300

    Update avatar user

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useRef } from 'react'
+import React, { ChangeEvent, useCallback, useRef } from 'react'
 import { FiMail, FiLock, FiUser, FiCamera, FiArrowLeft } from 'react-icons/fi'
 import { Form } from '@unform/web'
 import { FormHandles } from '@unform/core'
@@ -23,7 +23,7 @@ interface ProfileFormData {
 
 const Profile: React.FC = () => {
   const formRef = useRef<FormHandles>(null)
-  const { user } = useAuth()
+  const { user, updateUser } = useAuth()
 
   const { addToast } = useToast()
   const history = useHistory()
@@ -78,6 +78,25 @@ const Profile: React.FC = () => {
     [addToast, history],
   )
 
+  const handleAvatarChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      if (e.target.files) {
+        const data = new FormData()
+        data.append('avatar', e.target.files[0])
+
+        api.patch('/users/avatar', data).then((response) => {
+          updateUser(response.data)
+
+          addToast({
+            type: 'success',
+            title: 'Avatar atualizado com sucesso',
+          })
+        })
+      }
+    },
+    [addToast, updateUser],
+  )
+
   return (
     <Container>
       <header>
@@ -98,9 +117,10 @@ const Profile: React.FC = () => {
         >
           <AvatarInput>
             <img src={user.avatar_url} alt={user.name} />
-            <button type="button">
+            <label htmlFor="avatar">
               <FiCamera />
-            </button>
+              <input type="file" id="avatar" onChange={handleAvatarChange} />
+            </label>
           </AvatarInput>
 
           <h1>Meu perfil</h1>
