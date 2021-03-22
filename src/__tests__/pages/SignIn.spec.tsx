13c69416a9dbe849bf2230e69d63f05badf55c06
Bruno Henrique Magro <brunohmagro@hotmail.com>
2021-03-22 19:29:35 -0300

    Refactoring the test

--- src/__tests__/pages/SignIn.spec.tsx
+++ src/__tests__/pages/SignIn.spec.tsx
@@ -1,19 +1,32 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 
 import SignIn from '../../pages/SignIn'
+
+const mockedHistoryPush = jest.fn()
 
 jest.mock('react-router-dom', () => {
   return {
     Link: ({ children }: { children: React.ReactNode }) => children,
-    useHistory: jest.fn(),
+    useHistory: () => ({
+      push: mockedHistoryPush,
+    }),
   }
 })
 
 describe('SignIn Page', () => {
   it('should br able to sign in', () => {
-    const { debug } = render(<SignIn />)
+    const { getByPlaceholderText, getByText } = render(<SignIn />)
 
-    debug()
+    const emailField = getByPlaceholderText('E-mail')
+    const passwordField = getByPlaceholderText('Senha')
+    const buttonElement = getByText('Entrar')
+
+    fireEvent.change(emailField, { target: { value: 'johndoe@mail.com' } })
+    fireEvent.change(passwordField, { target: { value: '123456' } })
+
+    fireEvent.click(buttonElement)
+
+    expect(mockedHistoryPush).toHaveBeenCalledWith('/dashboard')
   })
 })
